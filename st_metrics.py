54ba6680cef12ff422d398dc673c396b1a422b34
ClRhlnd <60253580+ClRhlnd@users.noreply.github.com>
2023-06-11 20:55:53 +0200

    Add files via upload

---
+++ st_metrics.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.metric(label="Score", value="70 Points", delta="1.2 Points")
+
+df = pd.DataFrame({
+    'Rank': [1,2,3,4,5],
+    'Performer': [1,2,3,4,5],
+    'Points': [10,20,30,40,50]
+    })
+df.index.name = False
+st.write ('Hall of Fame', df)
