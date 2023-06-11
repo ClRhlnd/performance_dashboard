fefe95f1b8d4cd0fbe9894e467eaafc9ae1afd07
ClRhlnd <60253580+ClRhlnd@users.noreply.github.com>
2023-06-11 21:17:34 +0200

    Update st_metrics.py

--- st_metrics.py
+++ st_metrics.py
@@ -1,9 +1,18 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.header ('academic performance')
 st.metric(label="Score", value="70 Points", delta="1.2 Points")
 
+st.header ('learning curve')
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a','b','c']
+)
+st.line_chart(chart_data)
+
+st.header ('hall of fame')
 df = pd.DataFrame({
     'Rank': [1,2,3,4,5],
     'Performer': [1,2,3,4,5],
